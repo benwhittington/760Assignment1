02528842a059108e9ba29030fd899602ccc3cdab
benwhittington <benjiwhittington@gmail.com>
2020-03-29 13:42:02 +1300

    plot 3 random restarts

--- code/cpp/plot.py
+++ code/cpp/plot.py
@@ -7,11 +7,23 @@ if os.name == 'posix':
 
 
 def main():
-    obj = np.genfromtxt(path + "objTest")
+    bestObj = np.genfromtxt(path + "bestObj")
+    allObj = np.genfromtxt(path + "allObj") / 53843.4
+    bestObjIdx = np.genfromtxt(path + "bestObjIdx")
+    idx = np.genfromtxt(path + "indices", dtype=int)
 
     _, ax = plt.subplots(1)
     ax.set_yscale("log")
-    ax.plot(obj)
+    ax.scatter(np.arange(allObj.shape[0]), allObj, marker='x', color='k', s=0.1)
+    ax.plot(bestObjIdx[0:idx[0]], bestObj[0:idx[0]])
+    ax.plot(bestObjIdx[idx[0]:idx[1]], bestObj[idx[0]:idx[1]])
+    ax.plot(bestObjIdx[idx[1]:], bestObj[idx[1]:])
+    ax.set_ylim(3e-5, 10)
+
+    ax.set_title("Next Descent with 3 Random Restarts")
+    ax.set_ylabel("Objective")
+    ax.set_xlabel("Objective function Evaluations")
+
     plt.show()
 
 if __name__ == "__main__":
